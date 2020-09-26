86a0685ba619c80f20aa2c5050f85e355662bf94
Pablo Morcillo Fulgencio <PMF@nextret.net>
2020-09-26 21:38:52 +0200

    docs agregado

---
+++ docs/precache-manifest.b1e1014026a5996ecfe4e5d2dbecc5f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7612280abef075b9d608c9c5e645b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "455d623b7b0cca54d563",
+    "url": "/static/css/main.e2588b09.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455d623b7b0cca54d563",
+    "url": "/static/js/main.b9cc0c1f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
